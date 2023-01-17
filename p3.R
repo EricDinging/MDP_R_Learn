f15028688a6f8b74e05dc74780403d00be3bd9e9
EricDinging <ericdingg16@outlook.com>
2023-01-17 00:26:14 -0500

    data hl

--- p3.R
+++ p3.R
@@ -9,13 +9,12 @@ score <- function(symbols) {
     payouts <- c("DD" = 100, "7" = 80, "BBB" = 40, "BB" = 25, "B" = 10, "C" = 10, "0" = 0)
     prize <- unname(payouts[symbols[1]])
   } else if (all(symbols %in% c("B", "BB", "BBB"))) {
-    price <- 5
+    prize <- 5
   } else {
     cherries <- sum(symbols == "C")
     payouts <- c(0, 2, 5)
     prize <- payouts[cherries + 1]
   }
-  
   diamonds <- sum(symbols == "DD")
   prize * 2 ^ diamonds
 }
@@ -39,5 +38,25 @@ print.slots <- function(prize, ...){
   
   # display character string in console without quotes
   cat(string)
+}
+
+expectedValue <- function() {
+  wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+  prob = c("DD" = 0.03, "7" = 0.03, "BBB" = 0.06, "BB" = 0.1, "B" = 0.25,"C" = 0.01, "0" = 0.52)
+  combos <- expand.grid(wheel, wheel, wheel, stringsAsFactors = FALSE)
+  combos$prob1 <- prob[combos$Var1]
+  combos$prob2 <- prob[combos$Var2]
+  combos$prob3 <- prob[combos$Var3]
+  combos$prob <- combos$prob1 * combos$prob2 * combos$prob3
+  
+  combos$prizes <- NA
+  
+  for (i in 1:nrow(combos)) {
+    symbols <- c(combos[i, 1], combos[i, 2], combos[i, 3])
+    combos$prizes[i] <- score(symbols)
+  }
+  
+  sum(combos$prizes * combos$prob)
+  
 }
 
--- test4.R
+++ test4.R
@@ -1,20 +1,7 @@
-setup <- function(deck) {
-  
-  DEAL <- function() {
-    card <- deck[1, ]
-    assign("deck", deck[-1, ], envir = parent.env(environment()))
-    card
-  }
-  
-  SHUFFLE <- function(){
-    random <- sample(1:52, size = 52)
-    assign("deck", deck[random, ], envir = parent.env(environment()))
-  }
-  
-  list(deal = DEAL, shuffle = SHUFFLE)
-}
-
-cards <- setup(deck)
-deal <- cards$deal
-shuffle <- cards$shuffle
-
+rolls <- expand.grid(die, die)
+prob <- c("1" = 1/8, "2" = 1/8, "3" = 1/8, "4" = 1/8, "5" = 1/8, "6" = 3/8)
+rolls$value <- rolls$Var1 + rolls$Var2
+rolls$prob1 <- prob[rolls$Var1]
+rolls$prob2 <- prob[rolls$Var2]
+rolls$prob <- rolls$prob1 * rolls$prob2
+sum(rolls$value * rolls$prob)
